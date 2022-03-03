5ec3a45d88c690a2a5248e1c9a6b6887cebe401a
Zijun Yu <zijun_yu@foxmail.com>
2022-03-03 11:15:36 +0800

    20 shrink

--- passes/shrink.rkt
+++ passes/shrink.rkt
@@ -8,6 +8,9 @@
 
 (define (shrink-exp e)
   (match e
+    [(or (Int x) (Var x) (Bool x)) e]
+    [(Let x rhs body) (Let x (shrink-exp rhs) (shrink-exp body))]
+    [(If e1 e2 e3) (If (shrink-exp e1) (shrink-exp e2) (shrink-exp e3))]
     [(Prim 'and (list e1 e2))
      (If (shrink-exp e1)
          (shrink-exp e2)
@@ -16,4 +19,4 @@
      (If (shrink-exp e1)
          (Bool #t)
          (shrink-exp e2))]
-    [_ e]))
+    [(Prim op es) (Prim op (for/list ([e es]) (shrink-exp e)))]))
