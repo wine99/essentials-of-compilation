30dc84298d1d287f0bbf18db6dede6f007aba340
Zijun Yu <zijun_yu@foxmail.com>
2022-03-07 14:55:21 +0800

    Lif improve eplicate-controal: no trivial block anymore

--- passes/explicate-control.rkt
+++ passes/explicate-control.rkt
@@ -18,12 +18,15 @@
     [(If e1 e2 e3)
      (define-values (cont^ blocks^) (explicate-tail e2 blocks))
      (define-values (cont^^ blocks^^) (explicate-tail e3 blocks^))
-     (define thn-label (gensym "block"))
-     (define els-label (gensym "block"))
-     (explicate-pred e1 (Goto thn-label) (Goto els-label)
-                     (append `((,thn-label . ,cont^) (,els-label . ,cont^^))
-                             blocks^^))]
+     (explicate-pred e1 cont^ cont^^ blocks^^)]
     [_ (values (Return e) blocks)]))
+
+; Use this function to avoid repeating and also avoid trivial blocks
+(define (wrap-with-goto block blocks)
+  (cond
+    [(Goto? block) (values block blocks)]
+    [else (define label (gensym "block"))
+          (values (Goto label) (cons `(,label . ,block) blocks))]))
 
 (define (explicate-assign e x cont blocks)
   (match e
@@ -31,16 +34,10 @@
      (define-values (cont^ blocks^) (explicate-assign body x cont blocks))
      (explicate-assign rhs y cont^ blocks^)]
     [(If e1 e2 e3)
-     ; Avoid repeating cont
-     (define cont-label (gensym "block"))
-     (define blocks^ (cons `(,cont-label . ,cont) blocks))
-     (define-values (thn-cont blocks^^) (explicate-assign e2 x (Goto cont-label) blocks^))
-     (define-values (els-cont blocks^^^) (explicate-assign e3 x (Goto cont-label) blocks^^))
-     (define thn-label (gensym "block"))
-     (define els-label (gensym "block"))
-     (define blocks^^^^
-       (append `((,thn-label . ,thn-cont) (,els-label . ,els-cont)) blocks^^^))
-     (explicate-pred e1 (Goto thn-label) (Goto els-label) blocks^^^^)]
+     (define-values (cont^ blocks^) (wrap-with-goto cont blocks))
+     (define-values (els-cont blocks^^) (explicate-assign e3 x cont^ blocks^))
+     (define-values (thn-cont blocks^^^) (explicate-assign e2 x cont^ blocks^^))
+     (explicate-pred e1 thn-cont els-cont blocks^^^)]
     [_ (values (Seq (Assign (Var x) e) cont) blocks)]))
 
 ; The 'thn-block' and 'els-block' coming in is always a single goto
@@ -50,14 +47,15 @@
      (define-values (cont^ blocks^) (explicate-pred body thn-block els-block blocks))
      (explicate-assign rhs x cont^ blocks^)]
     [(If e1 e2 e3)
-     (define-values (cont^ blocks^) (explicate-pred e2 thn-block els-block blocks))
-     (define-values (cont^^ blocks^^) (explicate-pred e3 thn-block els-block blocks^))
-     (define thn-label (gensym "block"))
-     (define els-label (gensym "block"))
-     (explicate-pred e1 (Goto thn-label) (Goto els-label)
-                     (append `((,thn-label . ,cont^) (,els-label . ,cont^^))
-                             blocks^^))]
+     (define-values (els-block^ blocks^) (wrap-with-goto els-block blocks))
+     (define-values (thn-block^ blocks^^) (wrap-with-goto thn-block blocks^))
+     (define-values (cont^ blocks^^^) (explicate-pred e2 thn-block^ els-block^ blocks^^))
+     (define-values (cont^^ blocks^^^^) (explicate-pred e3 thn-block^ els-block^ blocks^^^))
+     (explicate-pred e1 cont^ cont^^ blocks^^^^)]
     [(Bool b) (values (if b thn-block els-block) blocks)]
     [(Var x) (explicate-pred (Prim 'eq? (list e (Bool #t))) thn-block els-block blocks)]
     [(Prim 'not (list e)) (explicate-pred e els-block thn-block blocks)]
-    [_ (values (IfStmt e thn-block els-block) blocks)]))
+    [_
+     (define-values (els-block^ blocks^) (wrap-with-goto els-block blocks))
+     (define-values (thn-block^ blocks^^) (wrap-with-goto thn-block blocks^))
+     (values (IfStmt e thn-block^ els-block^) blocks^^)]))
